9efce53364793f2802604f9fb03140a5165d1a88
Shane Avery Sistoza <me@ssistoza.dev>
2023-02-07 13:09:45 -0500

    align back button

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("If you see this! cdn is successful.")
+console.log('🔥🔥🔥If you see this! cdn is successful.🔥🔥🔥');
+
+function goBack() {
+  console.log('⬅️ Back button hit!');
+  return window.history.back();
+}
+
+const backButton = document.getElementsByClassName('return-back')[0];
+backButton.addEventListener('click', goBack);
